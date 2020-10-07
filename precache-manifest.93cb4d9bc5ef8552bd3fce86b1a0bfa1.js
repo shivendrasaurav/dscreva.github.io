780a1061005667585942829c468a6f030eeb6e9c
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-10-07 12:19:12 +0530

    Updates

---
+++ precache-manifest.93cb4d9bc5ef8552bd3fce86b1a0bfa1.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d7ce7c1efd9c3e7f46f2f6a4a45985",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62fccbc12c532e766cb7",
+    "url": "/static/css/2.6e3b96dd.chunk.css"
+  },
+  {
+    "revision": "ec9cdc34ef3d80d4e5fd",
+    "url": "/static/css/main.97b643aa.chunk.css"
+  },
+  {
+    "revision": "62fccbc12c532e766cb7",
+    "url": "/static/js/2.fa6e2cd9.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.fa6e2cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9cdc34ef3d80d4e5fd",
+    "url": "/static/js/main.b91b57a3.chunk.js"
+  },
+  {
+    "revision": "e5d746a64981e1e3b676",
+    "url": "/static/js/runtime-main.028b9b13.js"
+  },
+  {
+    "revision": "e548315a6759b9650f7ca195bba9c9a0",
+    "url": "/static/media/adarsh.e548315a.jpeg"
+  },
+  {
+    "revision": "1d45fe0d20cebac1463586cbf549278f",
+    "url": "/static/media/aman.1d45fe0d.jpeg"
+  },
+  {
+    "revision": "f73e0b22460530656ff1a368bf556cde",
+    "url": "/static/media/anurag.f73e0b22.jpeg"
+  },
+  {
+    "revision": "28494fa11166a00b322c34dfe0e992a0",
+    "url": "/static/media/chaitra.28494fa1.jpg"
+  },
+  {
+    "revision": "ded1e0edd58b2d97997d8f2cf8f9f6a0",
+    "url": "/static/media/chirag.ded1e0ed.jpeg"
+  },
+  {
+    "revision": "82a858c8e6360b847f3eb326a04a9346",
+    "url": "/static/media/deeksha.82a858c8.jpeg"
+  },
+  {
+    "revision": "d09270b86be9391ada2eb95e0a1dcce1",
+    "url": "/static/media/devanshu.d09270b8.jpg"
+  },
+  {
+    "revision": "e88fba09f11c11283f85d4dce4de3265",
+    "url": "/static/media/dharshan.e88fba09.jpg"
+  },
+  {
+    "revision": "462e4d2a6b069791a6cb36ceb7a06eec",
+    "url": "/static/media/divyanshi.462e4d2a.jpg"
+  },
+  {
+    "revision": "03a1ff3a0f5cf66f9ef2820461fcfc3a",
+    "url": "/static/media/himanshu.03a1ff3a.jpg"
+  },
+  {
+    "revision": "49892328f6e8b05fbb5612e15dccd37a",
+    "url": "/static/media/kiran.49892328.png"
+  },
+  {
+    "revision": "e8166c05a99c92a45fcbc5a022364aff",
+    "url": "/static/media/logo.e8166c05.svg"
+  },
+  {
+    "revision": "f1f718ae56bdc0e55c231d42abe3bff0",
+    "url": "/static/media/niran.f1f718ae.jpeg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/nofile.d41d8cd9.ext"
+  },
+  {
+    "revision": "7e846f71504f76d641a9d27b14d68bc9",
+    "url": "/static/media/pranav.7e846f71.png"
+  },
+  {
+    "revision": "99c79b6bc7ed6df5ffd5b76f73f7c051",
+    "url": "/static/media/sahil.99c79b6b.jpg"
+  },
+  {
+    "revision": "9f3aa73dab4dc11659a16c9742b6da6f",
+    "url": "/static/media/saket.9f3aa73d.jpg"
+  },
+  {
+    "revision": "65721724895165fd47aafbadc8ed145d",
+    "url": "/static/media/sanjana.65721724.jpeg"
+  },
+  {
+    "revision": "2ef76141f25fa017f6c1ffbab17796d7",
+    "url": "/static/media/shivendra.2ef76141.jpeg"
+  },
+  {
+    "revision": "3b9f8ccd8301824e139515231469aa75",
+    "url": "/static/media/shraddha.3b9f8ccd.jpg"
+  },
+  {
+    "revision": "d7c4e409daa1f206350fcfe07adc032d",
+    "url": "/static/media/shreevallabha.d7c4e409.jpeg"
+  },
+  {
+    "revision": "c292e9d765a7a6b78c0e6bea651dbfd5",
+    "url": "/static/media/shubham.c292e9d7.jpeg"
+  },
+  {
+    "revision": "60879918f0989de77bc50b96c5a8c1bc",
+    "url": "/static/media/slide1.60879918.JPG"
+  },
+  {
+    "revision": "0d74dc5a47e2dd019f68ebe69575f3c1",
+    "url": "/static/media/slide2.0d74dc5a.JPG"
+  },
+  {
+    "revision": "02277536fb925ae9e3a7e617c80c7cfa",
+    "url": "/static/media/slide3.02277536.JPG"
+  },
+  {
+    "revision": "b1c92f91722938a2c0a84e4ca2dbfff6",
+    "url": "/static/media/slide4.b1c92f91.JPG"
+  },
+  {
+    "revision": "d7f5c73cfbf4bc60c75d3240bb3bb7c3",
+    "url": "/static/media/slide5.d7f5c73c.JPG"
+  },
+  {
+    "revision": "e249c0ad8efe5a427482948b445f3d59",
+    "url": "/static/media/slide6.e249c0ad.JPG"
+  },
+  {
+    "revision": "df57f76e35eab19f7b947991daea7e65",
+    "url": "/static/media/slide7.df57f76e.JPG"
+  },
+  {
+    "revision": "3e88c2fec4ccfe898607e482c4de088c",
+    "url": "/static/media/slide8.3e88c2fe.JPG"
+  },
+  {
+    "revision": "ea0eeb94d3f0038b42221d52557393ae",
+    "url": "/static/media/vijay.ea0eeb94.jpg"
+  },
+  {
+    "revision": "5b0340b20f1e175a4f9e0d5e4f1054e1",
+    "url": "/static/media/vishnu.5b0340b2.jpg"
+  }
+]);
